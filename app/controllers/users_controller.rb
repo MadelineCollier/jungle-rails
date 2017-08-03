6f10be29fcc620fba1569c2cf4a894b72c09c6be
Madeline Collier <madel@telus.net>
2017-08-03 19:50:12 +0000

    Create actions/methods new and create in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :products, notice: 'Thanks for Signing Up!'
+    else
+      render :new, notice: 'Something went wrong'
+    end
   end
 
 end
