b21177dff20285adadf3e4e782d1e76575298224
Madeline Collier <madel@telus.net>
2017-08-05 01:17:21 +0000

    Create delete buttons for each review (only visibile to users who made that review)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,6 +12,13 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
+    redirect_to @product
+  end
+
   private
 
   def review_params
