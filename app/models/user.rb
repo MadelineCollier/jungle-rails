6f85e547d8adc3c8c62da954ff1b54e0d8d6890c
Madeline Collier <madel@telus.net>
2017-08-07 19:43:03 +0000

    Add validation to user model to ensure password is at least 3 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, uniqueness: { case_sensitive: false }
   validates :email, :first_name, :last_name, presence: true
+  validates :password, length: { minimum: 3 }
 end
