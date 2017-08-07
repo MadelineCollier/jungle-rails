e2b16589a72654b5281fd37cb9701d43db1690b7
Madeline Collier <madel@telus.net>
2017-08-07 19:04:03 +0000

    Create user model test for when user last_name is nil

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,5 +24,17 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
       expect{ raise StandardError, "can't be blank" }.to raise_error("can't be blank")
     end
+
+    it "is not valid without a last_name" do
+      @user = User.new(
+        first_name: "first-name",
+        last_name: nil,
+        email: "email@email.com",
+        password: "password",
+        password_confirmation: "password"
+      )
+      expect(@user).to_not be_valid
+      expect{ raise StandardError, "can't be blank" }.to raise_error("can't be blank")
+    end
   end
 end
