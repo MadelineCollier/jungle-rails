d7380b4f45d2c26a0bb2299e0b6c379260dc522e
Madeline Collier <madel@telus.net>
2017-08-07 20:03:01 +0000

    Create class method authenticate_with_credentials in user model to validate user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
+
   validates :email, uniqueness: { case_sensitive: false }
   validates :email, :first_name, :last_name, presence: true
   validates :password, length: { minimum: 3 }
+
+  def authenticate_with_credentials(email, password)
+    @email = email
+    @password = password
+    @user = User.find_by(@email)
+    if @user.authenticate(@password)
+      @user
+    else
+      nil
+    end
+  end
 end
