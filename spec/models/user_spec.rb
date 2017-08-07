4973b8ffec6fd07cd17921e6da306376203a8988
Madeline Collier <madel@telus.net>
2017-08-07 19:21:32 +0000

    Create user model test for when password and password_confirmation do not match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,5 +60,17 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
       expect{ raise StandardError, "can't be blank" }.to raise_error("can't be blank")
     end
+
+    it "is not valid when password and password_confirmation do not match" do
+      @user = User.new(
+        first_name: "first-name",
+        last_name: "last-name",
+        email: "email@email.com",
+        password: "1234",
+        password_confirmation: "12345"
+      )
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to eql  ["Password confirmation doesn't match Password"]
+    end
   end
 end
